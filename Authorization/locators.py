1c73d4d6ec1ff19d7af9da396b90afa986bb7fc2
Olga QA Engineer <gpv260783@gmail.com>
2026-02-01 19:11:01 +0300

    Add files via upload

---
+++ Authorization/locators.py
@@ -0,0 +1,4 @@
+class LoginPageLocators:
+    User_Name = ("xpath", "//input[@id='user-name']")
+    Password = ("xpath", "//input[@id='password']")
+    Login_button = ("xpath", "//input[@id='login-button']")
