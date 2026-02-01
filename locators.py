10e715eea8eed1b316dc8f44055f43650e10a810
Olga QA Engineer <gpv260783@gmail.com>
2026-02-01 19:20:43 +0300

    Delete locators.py

--- locators.py
+++
@@ -1,4 +0,0 @@
-class LoginPageLocators:
-    User_Name = ("xpath", "//input[@id='user-name']")
-    Password = ("xpath", "//input[@id='password']")
-    Login_button = ("xpath", "//input[@id='login-button']")
